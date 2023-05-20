21962e8a870deeb071ebe473de4111e538a5d41b
ahmedabdelhakim13 <ahmedhakem801@gmail.com>
2023-05-20 11:00:45 -0700

    main.c

--- main.c
+++ main.c
@@ -11,34 +11,40 @@ int main(int argc, char **argv)
 {
 	char cmd[MAX_CMD_LENGTH];
 
-	while(1){
+	while (1)
+	{
 		printf("simple_shell$");
-		if(fgets(cmd, MAX_CMD_LENGTH, stdin) == NULL){
+		if (fgets(cmd, MAX_CMD_LENGTH, stdin) == NULL)
+		{
 			printf("\n");
 			break;
 		}
-		//Remove trailing newline character
+		/*Remove trailing newline character*/
 		cmd[strcspn(cmd, "\n")] = '\0';
 
-		//Check for built-in commands
-		if(strcmp(cmd, "cd") == 0){
-			//Handle cd command
+		/*Check for built-in commands*/
+		if (strcmp(cmd, "cd") == 0)
+		{
+			/*Handle cd command*/
 			handle_cd_command();
 			continue;
-		}else if(strcmp(cmd, "exit") == 0){
-			//Handle exit command 
+		} else if (strcmp(cmd, "exit") == 0)
+		{
+		/*Handle exit command*/
 			handle_exit_command();
 			continue;
-		}else if (strcmp(cmd, "env") == 0){
-			//Handle env command
+		} else if (strcmp(cmd, "env") == 0)
+		{
+			/*Handle env command*/
 			handle_env_command();
 			continue;
 		}
 
-		//Execute command
-		if(!execute_command(cmd)){
+		/*Execute command*/
+		if (!execute_command(cmd))
+		{
 			continue;
 		}
 	}
-	return 0;
+	return (0);
 }
