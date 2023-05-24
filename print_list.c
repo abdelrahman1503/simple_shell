e5deabdff5d1c12a295a601bc0b7eb3d508e41eb
abdelrahman1503 <abdelrahman.1503h@gmail.com>
2023-05-24 20:43:52 +0300

    Linked List files 2

---
+++ print_list.c
@@ -0,0 +1,24 @@
+#include "simple_shell.h"
+
+/**
+ * print_list - prints all elements of a list_t linked list
+ * @h: pointer to first node
+ *
+ * Return: size of list
+ */
+size_t print_list(const st_list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
