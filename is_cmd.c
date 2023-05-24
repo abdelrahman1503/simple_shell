1ecce56ecd526f2f00452f6ceb9d1d5d82ea0421
abdelrahman1503 <abdelrahman.1503h@gmail.com>
2023-05-24 20:48:04 +0300

    Parser files

---
+++ is_cmd.c
@@ -0,0 +1,23 @@
+#include "simple_shell.h"
+
+/**
+ * is_cmd - determines if a file is an executable command
+ * @info: the info struct
+ * @path: path to the file
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int is_cmd(st_info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
