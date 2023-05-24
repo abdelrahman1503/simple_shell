684834438aa493906699d8fc6ffaec6f2d89a531
abdelrahman1503 <abdelrahman.1503h@gmail.com>
2023-05-24 20:18:14 +0300

    Environ files

---
+++ _unsetenv.c
@@ -0,0 +1,33 @@
+#include "simple_shell.h"
+
+/**
+ * _unsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: 1 on delete, 0 otherwise
+ * @var: the string env var property
+ */
+int _unsetenv(st_info_t *info, char *var)
+{
+	st_list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
---
+++ sigintHandler.c
@@ -0,0 +1,14 @@
+#include "simple_shell.h"
+
+/**
+ * sigintHandler - blocks ctrl-C
+ * @sig_num: the signal number
+ *
+ * Return: void
+ */
+void sigintHandler(__attribute__((unused))int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
+}
