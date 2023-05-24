d296343e595746c81deef7b9680689dc91f80b17
abdelrahman1503 <abdelrahman.1503h@gmail.com>
2023-05-24 20:50:02 +0300

    Memory allocation functions

---
+++ ffree.c
@@ -0,0 +1,16 @@
+#include "simple_shell.h"
+
+/**
+ * ffree - frees a string of strings
+ * @pp: string of strings
+ */
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
